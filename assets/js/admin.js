69031f95ba4adcebffe9e27a0b24f858ad84d3f4
ibadzadee <sara.ibadzade@gmail.com>
2024-02-04 22:07:25 +0400

    restaurants, login, sign up page draft

---
+++ assets/js/admin.js
@@ -0,0 +1,18 @@
+let logoffEl = document.querySelector('#logoff');
+let infoEl = document.querySelector('#info');
+let logoutBtn = document.querySelector('#logout');
+
+let user = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')).name : null;
+
+if(user){
+    infoEl.innerHTML = `<p>Welcome, ${user}!` 
+    infoEl.style.display = 'block'
+    logoffEl.style.display = 'none'
+}else{
+    setTimeout(()=>{window.location = './login.html'}, 2000)
+}
+
+logoutBtn.addEventListener('click',()=>{
+    localStorage.removeItem('currentUser')
+    window.location = './login.html'
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -4,5 +4,25 @@ let form =document.querySelector('#form')
 
 form.addEventListener('submit' , (e)=>{
     e.preventDefault();
-    
+    let emailInp = document.querySelector("#emailInp").value;
+    let passInp = document.querySelector("#passInp").value;
+    // let nameInp = document.querySelector("#nameInp").value;
+
+    fetch('http://localhost:3000/users', {
+      }).then(res=>res.json())
+      .then(data => {
+        console.log(data);
+
+        let currentUserInfo = data.find((user)=>user.email == emailInp)
+        if(currentUserInfo){
+            if(currentUserInfo.password == passInp){
+                localStorage.setItem('currentUser', JSON.stringify(currentUserInfo));
+                window.location = './admin.html'
+            }else{
+                console.log('Wrong password');
+            }      
+        }else{
+            console.log("Wrong email");
+        }
+      })
 })
