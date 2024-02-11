f2adc139283cecf23b3f37d020699fa8a541d907
ibadzadee <sara.ibadzade@gmail.com>
2024-02-11 16:21:16 +0400

    logo..

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,11 +1,11 @@
 let form = document.querySelector("#form");
 let url = `http://localhost:3000/users`;
-let signBtn = document.querySelector("#signUp2");
+let signBtn = document.querySelector("#signUpForm");
 
 console.log(signBtn);
 
 signBtn.addEventListener("submit", (e) => {
-  event.preventDefault();
+  e.preventDefault();
   let emailInp = document.querySelector("#email").value;
   let passInp = document.querySelector("#password").value;
   let nameInp = document.querySelector("#name").value;
@@ -14,21 +14,27 @@ signBtn.addEventListener("submit", (e) => {
     email: emailInp,
     password: passInp,
   };
-  if (emailInp.trim() || passInp.trim() || nameInp.trim() === "") {
-    alert("Please be careful!");
-  } else {
-    axios.post(url, obj).then((res) => {
-      console.log(res.data);
-      alert("successfully signed up");
+
+  fetch("http://localhost:3000/users")
+    .then((res) => res.json())
+    .then((data) => {
+      let existingUser = data.find((user) => user.email === emailInp);
+      if (existingUser) {
+        alert("This email already exists");
+      } else {
+        axios.post(url, obj).then((res) => {
+          console.log(res.data);
+          alert("Successfully Signed Up!");
+        });
+      }
     });
-  }
 });
 
 let loginBtn = document.querySelector("#login");
 console.log(loginBtn);
 
-loginBtn.addEventListener("submit", () => {
-  event.preventDefault();
+loginBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
   let emailLogin = document.querySelector("#emailLogin").value;
   let passLogin = document.querySelector("#passwordLogin").value;
 
@@ -65,15 +71,59 @@ signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
 
-// let respMenu = document.querySelector('#respMenu')
-// let register = document.querySelector('#register')
-// window.addEventListener("resize", () => {
-//   if (window.Width > 768) {
-//     respMenu.style.visibility = "hidden";
-//     register.style.visibility = "visible";
-//   }
-//   else{
-//     respMenu.style.visibility = "visible";
-//     register.style.visibility = "hidden";
-//   }
-// });
+
+
+
+// ----------------- Resp < 600px -----------------
+
+let signBtn2 = document.querySelector("#signUpForm2");
+
+console.log(signBtn);
+
+signBtn2.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let emailInp = document.querySelector("#email2").value;
+  let passInp = document.querySelector("#password2").value;
+  let nameInp = document.querySelector("#name2").value;
+  let obj = {
+    name: nameInp,
+    email: emailInp,
+    password: passInp,
+  };
+  axios.post(url, obj).then((res) => {
+    console.log(res.data);
+    alert("successfully signed up");
+  });
+});
+
+let loginBtn2 = document.querySelector("#login2");
+console.log(loginBtn);
+
+loginBtn2.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let emailLogin = document.querySelector("#emailLogin2").value;
+  let passLogin = document.querySelector("#passwordLogin2").value;
+
+  console.log(emailLogin);
+  fetch("http://localhost:3000/users")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      let currentUserInfo = data.find((user) => user.email == emailLogin);
+      console.log(currentUserInfo);
+      if (currentUserInfo) {
+        if (currentUserInfo.password == passLogin) {
+          localStorage.setItem("currentUser", JSON.stringify(currentUserInfo));
+          window.location = "./admin.html";
+        } else {
+          console.log("Wrong password");
+        }
+      } else {
+        console.log("Wrong email");
+      }
+    });
+});
+
+
+
